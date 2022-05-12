80cc94b71c65630a2fbe3968db9c523cb86204ac
fuzzymango <46225294+fuzzymango@users.noreply.github.com>
2022-05-12 12:42:51 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,5 +1,17 @@
 from AutoLoader import AutoLoader
+'''
+ENABLE THE AUTO LOADER
 
+If the below statements are confusing or if your gizmos are stored all
+around your .nuke folder, use loader = AutoLoader()
+
+AutoLoader takes two optional arguments. 1) a file path to your gizmos and 2) a
+nuke toolbar to load them on to.
+
+EXAMPLE:
+# this loads all the gizmos from the .nuke\gizmos folder into a toolbar called MyGizmos
 myToolbar = nuke.menu('Nodes').addMenu('MyGizmos')
-loader = AutoLoader(r"C:\Users\isaac.spiegel\.nuke\gizmos", myToolbar)
+loader = AutoLoader(toolbar = myToolbar, directory = r"C:\Users\isaac.spiegel\.nuke\gizmos")
+'''
+loader = AutoLoader()
 loader.populate_toolbar()
